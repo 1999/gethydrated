9ca6797c08d79a48f19f8c62846634358616bee0
Dmitrii Sorin <info@staypositive.ru>
2019-09-18 03:21:19 +1000

    Queue: listen to changes via longpoll

--- api/src/app.ts
+++ api/src/app.ts
@@ -6,8 +6,7 @@ import cors from 'cors';
 
 import config from './config';
 import syncDatabaseModels from './database';
-import sync from './routes/sync';
-import { assertQueueExists } from './queue';
+import { assertQueueExists, putIntoQueue, listenQueueChanges } from './queue';
 
 const logger = pino();
 const app = express();
@@ -23,12 +22,25 @@ async function main() {
     assertQueueExists(sqs),
   ]);
 
-  sqs.receiveMessage({ QueueUrl: config.sqs.queueUrl, WaitTimeSeconds: 20 }, (err, data) => {
-    console.log('receive', { err, data });
-  });
+  listenQueueChanges(sqs);
 
   app.use(cors({ origin: true, credentials: true }));
-  app.post('/sync', jsonBodyParser({ limit: '4Mb' }), sync(sqs));
+  app.post('/sync', jsonBodyParser({ limit: '4Mb' }), async (req, res, next) => {
+    // TODO validate incoming cards structure
+    // TODO sanitise incoming cards data (only use known props)
+    // TODO group imcoming cards by id
+    // TODO sort incoming cards by revision
+    // TODO insert: if id doesn't exist - store
+    // TODO insert: if [id, revision] exists ignore
+    // TODO is there existing card with this [id, revision]? yes - looks like a merge conflict
+
+    try {
+      await putIntoQueue(sqs, req.body.cards);
+      res.sendStatus(202);
+    } catch (err) {
+      next(err);
+    }
+  });
 
   app.listen(80, () => {
     logger.info(`App is listening to incoming connections on port 80`);
--- api/src/queue.ts
+++ api/src/queue.ts
@@ -1,6 +1,47 @@
 import AWS from 'aws-sdk';
 import config from './config';
 
+export const listenQueueChanges = async (sqs: AWS.SQS): Promise<void> => {
+  const poll = async (): Promise<AWS.SQS.Message[]> => {
+    const data = await sqs.receiveMessage({ QueueUrl: config.sqs.queueUrl, WaitTimeSeconds: 20 }).promise();
+    return data.Messages || [];
+  };
+
+  console.log('run polling...');
+
+  while (true) {
+    const messages = await poll();
+    console.log(messages);
+  }
+};
+
+export const putIntoQueue = async (sqs: AWS.SQS, cards: any): Promise<void> => {
+  for (const card of cards) {
+    // await Card.findOrCreate({
+    //   where: {
+    //     card_id: card.id,
+    //     revision: card.revision,
+    //   },
+    //   defaults: {
+    //     card_id: card.id,
+    //     revision: card.revision,
+    //     prev_revision: '', // TODO
+    //     created_at: card.created_at,
+    //     deleted: card.deleted,
+    //     title: card.title,
+    //     data: card, // TODO
+    //   },
+    // });
+
+    sqs.sendMessage({
+      MessageBody: JSON.stringify(card),
+      QueueUrl: config.sqs.queueUrl,
+    }, (err, data) => {
+      console.log('send message', { err, data });
+    });
+  }
+};
+
 export const assertQueueExists = async (sqs: AWS.SQS): Promise<string> => {
   const { QueueUrls } = await sqs.listQueues().promise();
   if (QueueUrls && QueueUrls.length) {
